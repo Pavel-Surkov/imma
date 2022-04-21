3aa16f332e76c536897dc2d23673721a2d4bb0fe
Pavel Surkov <surkovv.pavel@gmail.com>
2022-04-21 22:23:26 +0700

    adds mobile version of all nfts page

--- src/js/components/AllNftTable.tsx
+++ src/js/components/AllNftTable.tsx
@@ -1,26 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { ITableData } from '../helpers/nftTableData';
 import avatar from '../../assets/images/icons/avatar.svg';
 
 interface AllNftTableProps {
 	tableData: Array<ITableData>;
-	dateConvert: (date: Date) => string;
-	convertDateToString: (date: Date) => string;
+	allTableVisible: boolean;
 }
 
-export const AllNftTable = ({ tableData, dateConvert, convertDateToString }: AllNftTableProps) => {
+export const AllNftTable = ({ tableData, allTableVisible }: AllNftTableProps) => {
 	return (
 		<table className="table lifefeed-table">
 			<tbody className="table-body">
-				{tableData.map((row) => {
+				{tableData.map((row, i) => {
 					const shortToken: string = row.token.slice(0, 17) + '...';
 					const shortOwnerWallet: string = row.owner_wallet.slice(0, 9) + '...';
 
 					const lastPrice: number = Number(row.lastprice);
 
+					const maxNftsInTable: number = 8;
+
 					return (
-						<tr className="table-row" key={row.id}>
+						<tr
+							className="table-row"
+							key={row.id}
+							style={
+								i >= maxNftsInTable && !allTableVisible
+									? { display: 'none' }
+									: { display: 'flex' }
+							}
+						>
 							<td className="table-col">
 								<div className="video">
 									<img
@@ -33,7 +42,7 @@ export const AllNftTable = ({ tableData, dateConvert, convertDateToString }: All
 										alt=""
 									/>
 									<div className="video-play__wrapper">
-										<Link to={row.slug} className="video-play">
+										<Link to={`/allnft/${row.slug}`} className="video-play">
 											<svg
 												data-svg="play"
 												width="21"
@@ -50,7 +59,7 @@ export const AllNftTable = ({ tableData, dateConvert, convertDateToString }: All
 										</Link>
 									</div>
 									<div className="video-play__link">
-										<Link to={row.slug}>
+										<Link to={`/allnft/${row.slug}`}>
 											<svg
 												width="21"
 												height="9"
@@ -114,5 +123,118 @@ export const AllNftTable = ({ tableData, dateConvert, convertDateToString }: All
 				})}
 			</tbody>
 		</table>
+	);
+};
+
+interface AllNftMobileProps {
+	tableData: Array<ITableData>;
+	allTableVisible: boolean;
+}
+
+export const AllNftMobile = ({ tableData, allTableVisible }: AllNftMobileProps) => {
+	return (
+		<div className="all-nft_mobile">
+			{tableData.map((row, i) => {
+				const shortToken: string = row.token.slice(0, 13) + '...';
+				const shortOwnerWallet: string = row.owner_wallet.slice(0, 8) + '...';
+
+				const lastPrice: number = Number(row.lastprice);
+
+				const maxNftsInTable: number = 8;
+
+				return (
+					<div
+						className="lifefeed-mobile__item"
+						key={row.id}
+						style={
+							i >= maxNftsInTable && !allTableVisible
+								? { display: 'none' }
+								: { display: 'flex' }
+						}
+					>
+						<div className="lifefeed-mobile__video">
+							<div className="video">
+								<img
+									width="138"
+									className="video-preview"
+									src={row.image.quality1x}
+									srcSet={`${row.image.quality1x} 1x, ${
+										row.image.quality2x ? row.image.quality2x : ''
+									} 2x`}
+									alt=""
+								></img>
+								<div className="video-play__wrapper">
+									<Link to={`/allnft/${row.slug}`} className="video-play">
+										<svg
+											data-svg="play"
+											width="21"
+											height="26"
+											viewBox="0 0 21 26"
+											fill="none"
+											xmlns="http://www.w3.org/2000/svg"
+										>
+											<path
+												d="M0.200195 12.9473V2.55576C0.200195 0.98489 1.9281 0.0272036 3.26019 0.859764L19.8866 11.2513C21.1399 12.0346 21.1399 13.8599 19.8866 14.6433L3.26019 25.0348C1.9281 25.8673 0.200195 24.9096 0.200195 23.3388V12.9473Z"
+												fill="#D6FF7E"
+											/>
+										</svg>
+									</Link>
+								</div>
+								<div className="video-play__link">
+									<Link to={`/allnft/${row.slug}`}>
+										<svg
+											width="21"
+											height="9"
+											viewBox="0 0 21 9"
+											fill="none"
+											xmlns="http://www.w3.org/2000/svg"
+										>
+											<path
+												d="M0.107422 4.62582H20.1074M20.1074 4.62582L16.2979 1.02582M20.1074 4.62582L16.2979 8.22582"
+												stroke="white"
+												strokeWidth="0.84374"
+											/>
+										</svg>
+									</Link>
+								</div>
+							</div>
+							<img data-img="sign" src={row.sign} alt="sign" />
+						</div>
+						<div className="lifefeed-mobile__info">
+							<div className="lifefeed-mobile__info-block">
+								<h4 className="title">Signed and created by</h4>
+								<p>{row.author}</p>
+							</div>
+							<div className="lifefeed-mobile__info-block">
+								<h4 className="title">IMMA NFT {row.hash}</h4>
+								<p>{row.hash}</p>
+							</div>
+							<div className="lifefeed-mobile__info-block">
+								<p className="title">Original NFT token adress</p>
+								<p>
+									<a className="link" href={row.token_url}>
+										{shortToken}
+									</a>
+								</p>
+							</div>
+							<div className="lifefeed-mobile__info-block">
+								<p className="title">Current owner wallet</p>
+								<p>
+									<a className="link" href={row.owner_wallet_url}>
+										{shortOwnerWallet}
+									</a>
+								</p>
+							</div>
+							<div className="lifefeed-mobile__info-block">
+								<div className="table-col__wrapper">
+									<p className="title">Last Price</p>
+									<p>{lastPrice} ETH</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				);
+			})}
+		</div>
 	);
 };
--- src/js/pages/AllImmaNft.tsx
+++ src/js/pages/AllImmaNft.tsx
@@ -1,22 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { SortNft } from '../components/SortNft';
-import { AllNftTable } from '../components/AllNftTable';
+import { AllNftTable, AllNftMobile } from '../components/AllNftTable';
 import { tableData, dateConvert, convertDateToString } from '../helpers/nftTableData';
 
 export const AllImmaNft: React.FC = () => {
+	const [allTableVisible, setAllTableVisible] = useState<boolean>(false);
+
 	return (
 		<section className="nfts">
 			<div className="nfts-wrapper">
 				<div className="container">
 					<h1 className="title title_size-m nfts__title">All IMMA NFT</h1>
 					<SortNft />
-					<AllNftTable
-						tableData={tableData}
-						dateConvert={dateConvert}
-						convertDateToString={convertDateToString}
-					/>
+					<AllNftTable tableData={tableData} allTableVisible={allTableVisible} />
+					<AllNftMobile tableData={tableData} allTableVisible={allTableVisible} />
 					<div className="view-more">
-						<button type="button" className="view-more__btn">
+						<button
+							type="button"
+							className="view-more__btn"
+							onClick={() => setAllTableVisible(true)}
+							style={
+								allTableVisible ? { display: 'none' } : { display: 'inline-block' }
+							}
+						>
 							View more
 						</button>
 					</div>
