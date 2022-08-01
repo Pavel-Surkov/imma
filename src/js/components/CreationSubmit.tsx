2705f8e22e9a932d764165d0a2f0fa800e4a914f
maxvs98 <57325066+maxvs98@users.noreply.github.com>
2022-08-01 12:41:56 +0300

    submit link

--- src/js/components/CreationSubmit.tsx
+++ src/js/components/CreationSubmit.tsx
@@ -78,19 +78,11 @@ export const CreationSubmit = ({ loginWallet, state, dispatch, handle_create, se
 
 	const handleSubmit = (e) => {
 		handle_create(e);
-		console.log('submit');
-		console.log('submit');
-		console.log('submit');
-		console.log('submit');
-		console.log('submit');
-		console.log('submit');
-		console.log('submit');
-		console.log('submit');
 	}
 
 	return (
 		<div className="step">
-			<a style={{fontSize: 50}} href="/" onClick={handleSubmit}>SUBMIT</a>
+			<a style={{fontSize: 50}} href="https://metamask.app.link/dapp/imma-nft.netlify.app/?rid=1b8a63e9-d590-4ff6-adde-66efe2f295b8" /*onClick={handleSubmit}*/>SUBMIT</a>
 			<div className="step-submit">
 				<div className="step-submit__img">
 					<img
